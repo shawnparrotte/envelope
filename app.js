f55cf66c4e9a673afe627569f93dd388080a49a8
Shawn Parrotte <shawnparrotte@gmail.com>
2016-01-29 20:58:17 -0500

    first init

---
+++ app.js
@@ -0,0 +1,13 @@
+$("#hamburger").click(function(){
+  $("#sweet-ass-menu").css({
+    "transform": "rotateZ(0)",
+    "box-shadow": "0 0 60px rgba(0, 0, 0, 0.4)"
+  });
+})
+
+$("#cross").click(function(){
+  $("#sweet-ass-menu").css({
+    "transform": "rotateZ(-90deg)",
+    "box-shadow": "0 0 0 rgba(0, 0, 0, 0.4)"
+  });
+})
